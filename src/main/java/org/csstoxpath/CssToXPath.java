6d1ed50cd0618af2e5fb624c67846019d816fc7c
roman_iovlev <roman_iovlev@epam.com>
2018-01-19 09:41:15 +0300

    Initial commit

---
+++ src/main/java/org/csstoxpath/CssToXPath.java
@@ -0,0 +1,99 @@
+package org.csstoxpath;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.String.format;
+import static java.util.Arrays.asList;
+
+/**
+ * Created by Roman_Iovlev on 1/5/2018.
+ */
+public class CssToXPath {
+    public static String cssToXPath(String css) {
+        if (css == null || css.isEmpty()) return "";
+        int i = 0; int start; int length = css.length();
+        String result = "//";
+        while (i < length) {
+            char symbol = css.charAt(i);
+            if (isTagLetter(symbol)) {
+                start = i;
+                while(i < length && isTagLetter(css.charAt(i))) i++;
+                if (i == length) return result + css.substring(start);
+                result += css.substring(start, i);
+                continue;
+            }
+            if (symbol == ' ') {
+                result += "//"; i++; continue;
+            }
+            if (asList('.','#','[').contains(symbol)) {
+                List<String> attributes = new ArrayList<>();
+                while (i < length && css.charAt(i) != ' ') {
+                    switch (css.charAt(i)) {
+                        case '.':
+                            i++; start = i;
+                            while (i < length && isAttrLetter(css.charAt(i))) i++;
+                            attributes.add(convertToClass(i == length
+                                ? css.substring(1)
+                                : css.substring(start, i)));
+                            break;
+                        case '#':
+                            i++; start = i;
+                            while (i < length && isAttrLetter(css.charAt(i))) i++;
+                            attributes.add(convertToId(i == length
+                                ? css.substring(1)
+                                : css.substring(start, i)));
+                            break;
+                        case '[':
+                            i++; String attribute = "@";
+                            while (i < length && (!asList('=',']').contains(css.charAt(i)))) {
+                                attribute += css.charAt(i);
+                                i++;
+                            }
+                            if (css.charAt(i) == '=') {
+                                attribute += "=";
+                                i++;
+                                if (css.charAt(i) != '\'') attribute += "'";
+                                while (i < length && css.charAt(i) != ']') {
+                                    attribute += css.charAt(i);
+                                    i++;
+                                }
+                                if (i == length)
+                                    throw new RuntimeException("Incorrect Css. No ']' symbol");
+                                if (attribute.charAt(attribute.length() - 1) != '\'') attribute += "'";
+                            }
+                            attributes.add(attribute);
+                            break;
+                        default: throw new RuntimeException(format("Can't process Css. Unexpected symbol %s in attributes", css.charAt(i)));
+                    }
+                    i++;
+                }
+                if (result.charAt(result.length()-1) == '/') result += "*";
+                result += "[" + String.join(" and ", attributes)+ "]";
+                continue;
+            }
+            throw new RuntimeException(format("Can't process Css. Unexpected symbol '%s'", symbol));
+        }
+        return result;
+    }
+    private static String convertToClass(String value) {
+        return "contains(@class,'" + value + "')";
+    }
+    private static String convertToId(String value) {
+        return convertToAtribute("id", value);
+    }
+    private static String convertToAtribute(String attr, String value) {
+        return "@" + attr + "='" + value + "'";
+    }
+    private static boolean isAttrLetter(char symbol) {
+        return symbol >= 'a' && symbol <= 'z' ||
+            symbol >= 'A' && symbol <= 'Z' ||
+            symbol >= '0' && symbol <= '9' ||
+            symbol == '-' || symbol == '_' || symbol == '.'
+            || symbol == ':';
+    }
+    private static boolean isTagLetter(char symbol) {
+        return symbol >= 'a' && symbol <= 'z';
+    }
+
+}
---
+++ src/main/java/org/csstoxpath/Tests.java
@@ -0,0 +1,31 @@
+package org.csstoxpath;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.csstoxpath.CssToXPath.cssToXPath;
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by Roman_Iovlev on 1/17/2018.
+ */
+public class Tests {
+    @Test
+    public void convertTest() {
+        assertEquals(cssToXPath("div"), "//div");
+        assertEquals(cssToXPath(".test-class"), "//*[contains(@class,'test-class')]");
+        assertEquals(cssToXPath("#test-id"), "//*[@id='test-id']");
+        assertEquals(cssToXPath("[type=testtype]"), "//*[@type='testtype']");
+        assertEquals(cssToXPath("[data-type=testdata]"), "//*[@data-type='testdata']");
+        assertEquals(cssToXPath("[class='test data cl']"), "//*[@class='test data cl']");
+        assertEquals(cssToXPath("[data='test-data']"), "//*[@data='test-data']");
+        assertEquals(cssToXPath("a[value=testvalue]"), "//a[@value='testvalue']");
+        assertEquals(cssToXPath("[class=test][type=submit]"), "//*[@class='test' and @type='submit']");
+        assertEquals(cssToXPath("div span"), "//div//span");
+        assertEquals(cssToXPath("[class=testclass] button"), "//*[@class='testclass']//button");
+        assertEquals(cssToXPath("a"), "//a");
+        assertEquals(cssToXPath("a div span button"), "//a//div//span//button");
+        assertEquals(cssToXPath("div[type=text][checked=false] span[dt-args='t b p']"), "//div[@type='text' and @checked='false']//span[@dt-args='t b p']");
+        assertEquals(cssToXPath(".open [data-toggle]"), "//*[contains(@class,'open') and @data-toggle]");
+    }
+}
